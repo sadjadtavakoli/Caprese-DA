4b358bddb55aebc9554dd49fdeebc717b63a7492
sadjadtavakoli <sadjadtavakoli@gmail.com>
2021-03-12 16:43:26 -0800

    expectedOutputs for RequireModule test

---
+++ test/expectedOutputs/functionCallRequireModule.js
@@ -0,0 +1,6 @@
+
+#1 function require is called with variables args by functionCallRequireModule.js
+#4 function callee is called with variables args by functionCallRequireModule.js
+#1 function callee entered with variables args from functionCallRequireModule.js
+#1 function callee exited with return values undefined to function functionCallRequireModule.js
+#end Execution
