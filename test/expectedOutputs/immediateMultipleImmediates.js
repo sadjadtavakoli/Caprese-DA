41dcf807b5043f29eca78b413b44355cd812bc4a
sadjadtavakoli <sadjadtavakoli@gmail.com>
2021-03-12 22:45:29 -0800

    fix wrongly imported files in previous commit

---
+++ test/expectedOutputs/immediateMultipleImmediates.js
@@ -0,0 +1,14 @@
+
+#1 function setImmediate1 is called with variables args by immediateMultipleImmediates.js
+#3 function setImmediate3 is called with variables args by immediateMultipleImmediates.js
+#7 function setImmediate7 is called with variables args by immediateMultipleImmediates.js
+#9 function setImmediate9 is called with variables args by immediateMultipleImmediates.js
+#17 function myFunction2 entered with variables args from setImmediate1
+#17 function myFunction2 exited with return values undefined to function setImmediate1
+#3 function anonymous3 entered with variables args from setImmediate3
+#3 function anonymous3 exited with return values undefined to function setImmediate3
+#13 function myFunction1 entered with variables args from setImmediate7
+#13 function myFunction1 exited with return values undefined to function setImmediate7
+#9 function anonymous9 entered with variables args from setImmediate9
+#9 function anonymous9 exited with return values undefined to function setImmediate9
+#end Execution
