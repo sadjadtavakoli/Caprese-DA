52d2a27c897a0c698335d243d2557e8e57141511
sadjadtavakoli <sadjadtavakoli@gmail.com>
2021-08-11 20:12:24 -0700

    update tests regarding string transactions

--- clasp/src/test/java/FirstTest.java
+++ clasp/src/test/java/FirstTest.java
@@ -15,33 +15,28 @@ class FirstTest {
 
     @Test
     void testReqularOne() throws IOException {
-        List<String> itemConstraint = Arrays.asList("6", "7", "8");
-        String[] sequences = {
-            "1 -1 6 1 2 3 -1 1 3 -1 4 -1 3 -1 -2", 
-            "1 4 -1 3 -1 2 3 -1 1 5 -1 -2",
-            "5 6 -1 1 2 -1 4 -1 3 -1 2 -1 -2", 
-            "5 -1 7 -1 1 6 -1 3 -1 2 -1 3 -1 -2" };
+        List<String> itemConstraint = Arrays.asList("f", "g", "h");
+        String[] sequences = { 
+            "a -1 f a b c -1 a c -1 d -1 c -1 -2", 
+            "a d -1 c -1 b c -1 a e -1 -2",
+            "e f -1 a b -1 d -1 c -1 b -1 -2", 
+            "e -1 7 -1 a f -1 c -1 b -1 c -1 -2" };
 
-        String[] expectedOutput = { 
-            "6 -1 1 -1 #SUP: 2",
-            "6 -1 2 -1 #SUP: 2",
-            "6 -1 3 -1 #SUP: 3",
-            "6 -1 4 -1 #SUP: 2" };
-
+        String[] expectedOutput = { "f -1 c -1 #SUP: 3", "f -1 b -1 c -1 #SUP: 2", "f -1 c -1 b -1 #SUP: 2",
+                "f -1 c -1 c -1 #SUP: 2", "f -1 a -1 d -1 c -1 #SUP: 2" };
+`
         List<String> result = AlgoCM_ClaSPExecutor.runList(itemConstraint, 0.5, sequences, null);
+        System.out.println(result);
         assertArrayEquals(expectedOutput, result.toArray());
     }
 
     @Test
     void testReptitivePatternNoItemset() throws IOException {
-        List<String> itemConstraint = Arrays.asList("6");
-        String[] sequences = {
-            "1 -1 6 -1 3 -1 4 -1 4 -1 -2", 
-            "1 -1 6 -1 3 -1 4 -1 4 -1 -2", 
-            "1 -1 6 -1 3 -1 4 -1 4 -1 -2", 
-            "1 -1 6 -1 3 -1 4 -1 4 -1 -2"};
+        List<String> itemConstraint = Arrays.asList("f");
+        String[] sequences = { "a -1 f -1 c -1 d -1 d -1 -2", "a -1 f -1 c -1 d -1 d -1 -2",
+                "a -1 f -1 c -1 d -1 d -1 -2", "a -1 f -1 c -1 d -1 d -1 -2" };
 
-        String[] expectedOutput = {"1 -1 6 -1 3 -1 4 -1 4 -1 #SUP: 4"};
+        String[] expectedOutput = { "a -1 f -1 c -1 d -1 d -1 #SUP: 4" };
 
         List<String> result = AlgoCM_ClaSPExecutor.runList(itemConstraint, 0.5, sequences, null);
         System.out.println(result.toString());
@@ -50,20 +45,16 @@ class FirstTest {
 
     @Test
     void testReptitivePatternItemset() throws IOException {
-        List<String> itemConstraint = Arrays.asList("6");
-        String[] sequences = {
-            "1 4 -1 6 -1 3 4 -1 4 -1 -2", 
-            "1 4 -1 6 -1 3 4 -1 4 -1 -2", 
-            "1 4 -1 6 -1 3 4 -1 4 -1 -2", 
-            "1 4 -1 6 -1 3 4 -1 4 -1 -2"};
+        List<String> itemConstraint = Arrays.asList("f");
+        String[] sequences = { "a d -1 f -1 c d -1 d -1 -2", "a d -1 f -1 c d -1 d -1 -2", "a d -1 f -1 c d -1 d -1 -2",
+                "a d -1 f -1 c d -1 d -1 -2" };
 
-        String[] expectedOutput = {"1 4 -1 6 -1 3 4 -1 4 -1 #SUP: 4"};
+        String[] expectedOutput = { "a d -1 f -1 c d -1 d -1 #SUP: 4" };
 
         List<String> result = AlgoCM_ClaSPExecutor.runList(itemConstraint, 0.5, sequences, null);
         System.out.println(result.toString());
         assertArrayEquals(expectedOutput, result.toArray());
     }
-
 
     @Test
     void testOverlappedPatternItemset() throws IOException {
