b08712e94b910a1dea35dc60160d7a63349c92fd
sadjadtavakoli <sadjadtavakoli@gmail.com>
2022-05-30 20:21:28 -0700

    summarized version of evaluation results

--- computeBerkeResult.js
+++ computeBerkeResult.js
@@ -14,36 +14,35 @@ function computeBerkeResult(changes) {
 }
 
 function sortAndReport(impactSet) {
-    // console.log(" ... Saving result ... ");
 
     let sortableImpactSet = [];
     for (var item of impactSet) {
-        sortableImpactSet.push(item);
+        sortableImpactSet.push({...{"consequent":item[0]}, ...item[1]});
     }
 
     sortableImpactSet.sort(function (a, b) {
-        if ((a[1]['DA'] && !b[1]['DA'])) {
+        if ((a['DA-antecedents'] && !b['DA-antecedents'])) {
             return -1;
         }
-        if (b[1]['DA'] && !a[1]['DA']) {
+        if (b['DA-antecedents'] && !a['DA-antecedents']) {
             return 1;
         }
-        if (a[1]['FP'] && !b[1]['FP']) {
+        if (a['FP-antecedents'] && !b['FP-antecedents']) {
             return -1;
         }
-        if (b[1]['FP'] && !a[1]['FP']) {
+        if (b['FP-antecedents'] && !a['FP-antecedents']) {
             return 1;
         }
 
-        let aFP = a[1]['FP'] || { 'score': 0 };
-        let bFP = b[1]['FP'] || { 'score': 0 };
-        if (aFP['score'] - bFP['score'] != 0) {
-            return bFP['score'] - aFP['score'];
+        let aFP = a['FP-score'] || { 'FP-score': 0 };
+        let bFP = b['FP-score'] || { 'FP-score': 0 };
+        if (aFP['FP-score'] - bFP['FP-score'] != 0) {
+            return bFP['FP-score'] - aFP['FP-score'];
         }
 
-        let aDA = a[1]['DA'] || { 'score': 0 };
-        let bDA = b[1]['DA'] || { 'score': 0 };
-        return bDA['score'] - aDA['score'];
+        let aDA = a['DA-antecedents']|| { 'DA-antecedents': [] };
+        let bDA = b['DA-antecedents'] || {'DA-antecedents': [] };
+        return bDA.length - aDA.length;
     });
     fs.writeFileSync(constants.Berke_RESULT_PATH, JSON.stringify(sortableImpactSet));
 }
@@ -66,24 +65,20 @@ function intrepretDAResult(changes, impactSet) {
             }
 
             for (let test of dependencies['tests']) {
-                addDAImpactSet(keyMap[test], changedFucntion, true);
+                addDAImpactSet(keyMap[test], changedFucntion);
 
             }
         }
     }
 
-    function addDAImpactSet(item, antecedent, isTestFunction) {
+    function addDAImpactSet(item, antecedent) {
         if (!changes.includes(item)) {
-            if (impactSet.has(item) && impactSet.get(item)['DA']) {
-                impactSet.get(item)['DA']['antecedents'].push(antecedent);
+            if (impactSet.has(item) && impactSet.get(item)['DA-antecedents']) {
+                impactSet.get(item)['DA-antecedents'].push(antecedent);
             }
             else {
-                let itemInfo = {};
-                itemInfo['DA'] = { 'antecedents': [antecedent] };
-                impactSet.set(item, itemInfo);
+                impactSet.set(item, { 'DA-antecedents': [antecedent] });
             }
-            if (isTestFunction)
-                impactSet.get(item)['DA']['test'] = isTestFunction;
         }
     }
 }
@@ -95,9 +90,11 @@ function intrepretFPData(changes, impactSet) {
     let removed = fs.readFileSync(constants.REMOVED_PATH).toString().split(", ");
 
     patterns = patterns.split(",");
+    patterns.pop()
     for (let pattern of patterns) {
         let sequence = pattern.split(" -1:")[0];
-        let probability = pattern.split(" -1:")[1];
+        let probability = pattern.split(" -1:")[1].split(" ")[0];
+        let support = pattern.split(" -1:")[1].split(" ")[1];
         /*
         Keep the number of change-set items included in that particular itemset
         */
@@ -108,16 +105,16 @@ function intrepretFPData(changes, impactSet) {
             let newScore = probability;
             if (impactSet.has(transaction)) {
                 let scores = impactSet.get(transaction);
-                if (!scores['FP'] || (scores['FP'] && newScore > scores['FP']['score'])) {
-                    scores['FP'] = { 'score': newScore, 'antecedents': [intersections] };
-                } else if (newScore == scores['FP']['score']) {
-                    if (!scores['FP']['antecedents'].includes(intersections)) {
-                        scores['FP']['antecedents'].push(intersections);
+                if (!scores['FP-score'] || (scores['FP-score'] && newScore > scores['FP-score'])) {
+                    scores = { 'FP-score': newScore, "support": support, 'FP-antecedents': [intersections] };
+                } else if (newScore == scores['FP-score']) {
+                    if (!scores['FP-antecedents'].includes(intersections)) {
+                        scores['FP-antecedents'].push(intersections);
                     }
                 }
                 impactSet.set(transaction, scores); // @TODO we are ignoring number of items included in this change-sets in our result ordering
             } else {
-                impactSet.set(transaction, { 'FP': { 'score': newScore, 'antecedents': [intersections] } }); // @TODO we are ignoring number of items included in this change-sets in our result ordering
+                impactSet.set(transaction, { 'FP-score': newScore, 'support': support, 'FP-antecedents': [intersections] }); // @TODO we are ignoring number of items included in this change-sets in our result ordering
             }
         }
     }
--- evaluation/evaluation.js
+++ evaluation/evaluation.js
@@ -3,61 +3,63 @@ const fs = require('fs');
 const path = require('path')
 const { exec } = require('child_process');
 const { evaluationGetMainData, evaluationAnalyzer } = require('../berke');
-const { range } = require('lodash');
 
 const NUMBER_OF_COMMITS_PER_PROJECT = 10;
-const CLEANUP_COMMAND = "node cleanup.js";
-// const CLEANUP_COMMAND = "node -v";
-const RESULT_DIR_PATH = `${__dirname}${path.sep}result${path.sep}${constants.PROJECT_NAME}${path.sep}contribution`;
+const RESULT_DIR_PATH = `${__dirname}${path.sep}result${path.sep}${constants.PROJECT_NAME}`;
 
 const TARMAQ_PATH = path.dirname(path.dirname(__dirname)) + path.sep + "TARMAQ";
 const TARMAQ_COMMAND = "cd " + TARMAQ_PATH + " ; mvn exec:java -Dexec.mainClass='TARMAQ.MainTARMAQ' -Dexec.args=";
 
-const COMMIT_DATA_PATH = `${RESULT_DIR_PATH}${path.sep}commits.json`
-const RESULT_JSON_PATH = `${RESULT_DIR_PATH}${path.sep}results.json`
+const RESULT_PATH = `${RESULT_DIR_PATH}${path.sep}results.json`
+const UNITS_CONTRIBUTION_PATH = `${RESULT_DIR_PATH}${path.sep}units_contribution.json`
 const TARMAQ_RESULT_PATH = `${RESULT_DIR_PATH}${path.sep}tarmaq.json`
 
-
-if (!fs.existsSync(RESULT_DIR_PATH)) {
-    fs.mkdirSync(RESULT_DIR_PATH, {
-        recursive: true
-    });
+const STATUS = {
+    berke_unique: "Berke Unique",
+    tarmaq_unique: "TARMAQ Unique",
+    common: "common",
+    removed: "Removed"
 }
 
-if (fs.existsSync(RESULT_JSON_PATH)) {
-    fs.unlinkSync(RESULT_JSON_PATH)
-}
-fs.writeFileSync(RESULT_JSON_PATH, "{}")
+if (process.argv[1].endsWith(path.basename(__filename))) {
 
-exec(CLEANUP_COMMAND, (err, stdout, stderr) => {
-    if (!err) {
-        evaluationGetMainData(constants.SEED_COMMIT).then(testSetGenerator)
-            .then((candidatedCommits) => {
-                let testSet = [...candidatedCommits.keys()]
-                testSet = testSet.reduce(
-                    (p, x) => p.then(() => {
-                        return evaluationAnalyzer(x).then(() => runTARMAQ(x)).then(() => getUniqueContributions(candidatedCommits.get(x)))
-                    }),
-                    Promise.resolve())
-                return testSet
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    } else {
-        console.log(err)
+    if (!fs.existsSync(RESULT_DIR_PATH)) {
+        fs.mkdirSync(RESULT_DIR_PATH, {
+            recursive: true
+        });
     }
-})
+    
+    if (fs.existsSync(UNITS_CONTRIBUTION_PATH)) {
+        fs.unlinkSync(UNITS_CONTRIBUTION_PATH)
+    }
+    fs.writeFileSync(UNITS_CONTRIBUTION_PATH, "{}")
+    
+    evaluationGetMainData(constants.SEED_COMMIT)
+        .then(testSetGenerator)
+        .then((candidatedCommits) => {
+            let testSet = [...candidatedCommits.keys()]
+            testSet = testSet.reduce(
+                (p, x) => p.then(() => {
+                    return evaluationAnalyzer(x)
+                        .then(() => runTARMAQ(x))
+                        .then(() => collectResult(candidatedCommits.get(x)))
+                }),
+                Promise.resolve())
+            return testSet
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
 function testSetGenerator() {
     return new Promise(resolve => {
         console.log(" * * * testset Generator * * * ")
         let detailedSequences = fs.readFileSync(constants.SEQUENCES_PATH + "details.txt").toString().trim().split("\n");
-        let sequences = fs.readFileSync(constants.SEQUENCES_PATH).toString().trim().split("\n");
         let removed = fs.readFileSync(constants.REMOVED_PATH).toString().split(", ");
         let candidatedCommits = new Map()
-        let randomsSoFar = []
-        let maxIndex = detailedSequences.length
+        let maxIndex = detailedSequences.length - 1
+        console.log(`maxIndex = ${maxIndex}`)
         let useLessFiles = [
             "history.md", "HISTORY.md", "History.md",
             "README.md", "readme.md", "Readme.md",
@@ -66,11 +68,8 @@ function testSetGenerator() {
 
         while (true) {
             let i = getRandomNumbers(maxIndex)
-            while (randomsSoFar.length > 0 && randomsSoFar.includes(i)) {
-                i = getRandomNumbers(maxIndex)
-            }
-            randomsSoFar.push(i)
-
+            maxIndex -= 1
+            console.log(`random i = ${i}`)
             let sequence = detailedSequences[i]
             let commit = sequence.split(" : ")[0]
             let commitChanges = sequence.split(" : ")[1].slice(0, -4).split(" ").filter(item => !removed.includes(item) & !useLessFiles.includes(item))
@@ -81,60 +80,98 @@ function testSetGenerator() {
                 break
             }
             candidatedCommits.set(commitChanges, commit)
-            sequences.splice(i, 1)
+            detailedSequences.splice(i, 1)
         }
+
         let reverseMap = new Map()
         for (let entity of candidatedCommits) {
             reverseMap.set(entity[1], { "commits": entity[0] })
         }
 
-        fs.writeFileSync(COMMIT_DATA_PATH, JSON.stringify(Object.fromEntries(reverseMap)))
-        fs.writeFileSync(constants.SEQUENCES_PATH, sequences.join("\n"));
+        detailedSequences = detailedSequences.map(item => item.split(" : ")[1])
+
+        fs.writeFileSync(RESULT_PATH, JSON.stringify(Object.fromEntries(reverseMap)))
+        fs.writeFileSync(constants.SEQUENCES_PATH, detailedSequences.join("\n"));
         resolve(candidatedCommits)
     })
 }
 
-function getRandomNumbers(maximum) {
-    return Math.floor(Math.random() * maximum)
-}
-function getUniqueContributions(commit) {
+function collectResult(commit) {
     console.log(" = = = Collect Result = = = ")
 
-    // collect and evaluate Berke's result
-    let impactSet = JSON.parse(fs.readFileSync(constants.Berke_RESULT_PATH));
-    let contributionResult = JSON.parse(fs.readFileSync(RESULT_JSON_PATH));
-    let uniqeContributions = { 'DA': [], 'FP': [], 'Common': [] }
-    for (let item of impactSet) {
-        if (Object.keys(item[1]).length == 1) {
-            uniqeContributions[Object.keys(item[1])[0]].push(item[0])
-        } else {
-            uniqeContributions['Common'].push(item[0])
-        }
-    }
-    contributionResult[commit] = uniqeContributions
-    fs.writeFileSync(RESULT_JSON_PATH, JSON.stringify(contributionResult))
+    let commitsInfo = JSON.parse(fs.readFileSync(RESULT_PATH));
 
-
+    // separate berke's different units' results
+    let uniqeContributions = separateDAnFPsResults(commit);
 
     // collect and evaluate TARMAQ's resutl
+    let tarmaqResult = tarmaqConsequentStatusUpdate(uniqeContributions);
+    commitsInfo[commit]['tarmaq'] = tarmaqResult
+
+    // collect and evaluate Berke's result
+    commitsInfo[commit]['berke'] = berkeConsequentStatusUpdate(tarmaqResult);
+
+    fs.writeFileSync(RESULT_PATH, JSON.stringify(commitsInfo))
+
+}
+
+function tarmaqConsequentStatusUpdate(berkeSeparateUnitsResult) {
     let tarmaqResult = JSON.parse(fs.readFileSync(TARMAQ_RESULT_PATH));
     let removed = fs.readFileSync(constants.REMOVED_PATH).toString().split(", ");
     tarmaqResult = tarmaqResult.map(item => {
-        let consequent = item['rule'].split(" => ")[1]
-        if(removed.includes(consequent)){
-            item['status'] = "removed"
+        let consequent = item['rule'].split(" => ")[1];
+        item['consequent'] = consequent;
+        if (removed.includes(consequent)) {
+            item['status'] = STATUS.removed;
+        } else if (berkeSeparateUnitsResult['FP'].includes(consequent)) {
+            item['status'] = STATUS.common;
+        } else {
+            item['status'] = STATUS.tarmaq_unique;
         }
-        if(uniqeContributions['FP'].includes(consequent)){
-            item['status'] = "common with Berke"
-        }
-        return item
+        return item;
+    });
+    return tarmaqResult;
+}
+
+function berkeConsequentStatusUpdate(tarmaqResult) {
+    let impactSet = JSON.parse(fs.readFileSync(constants.Berke_RESULT_PATH));
+    let tarmaqKeys = tarmaqResult.map(item => {
+        return item['consequent'];
     });
 
-    let commitsInfo = JSON.parse(fs.readFileSync(COMMIT_DATA_PATH));
-    commitsInfo[commit]['berke'] = impactSet
-    commitsInfo[commit]['tarmaq'] = tarmaqResult
-    fs.writeFileSync(COMMIT_DATA_PATH, JSON.stringify(commitsInfo))
+    impactSet = impactSet.map(item => {
+        if (tarmaqKeys.includes(item["consequent"])) {
+            item["status"] = STATUS.common;
+        } else {
+            item["status"] = STATUS.berke_unique;
+        }
+        return item;
+    });
+    return impactSet;
+}
 
+function separateDAnFPsResults(commit) {
+    let impactSet = JSON.parse(fs.readFileSync(constants.Berke_RESULT_PATH));
+    let commitsContributionData = JSON.parse(fs.readFileSync(UNITS_CONTRIBUTION_PATH));
+    let uniqeContributions = { 'DA': [], 'FP': [], 'Common': [] };
+
+    for (let item of impactSet) {
+        if (item["FP-antecedents"] != undefined && item["DA-antecedents"] == undefined) {
+            uniqeContributions["FP"].push(item["consequent"]);
+        } else if (item["FP-antecedents"] == undefined && item["DA-antecedents"] != undefined) {
+            uniqeContributions["DA"].push(item["consequent"]);
+        }
+        else {
+            uniqeContributions['Common'].push(item["consequent"]);
+        }
+    }
+    commitsContributionData[commit] = uniqeContributions;
+    fs.writeFileSync(UNITS_CONTRIBUTION_PATH, JSON.stringify(commitsContributionData));
+    return uniqeContributions;
+}
+
+function getRandomNumbers(maximum) {
+    return Math.floor(Math.random() * maximum)
 }
 
 function areEquals(array1, array2) {
@@ -168,3 +205,5 @@ function runTARMAQ(changeSet) {
         })
     })
 }
+
+module.exports = { STATUS }
