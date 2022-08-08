83639d296671a05d2f7fe8aa5a27efb465f5a1c7
sadjadtavakoli <sadjadtavakoli@gmail.com>
2022-08-08 12:35:57 -0700

    runner running bug fixed + project configuration instead of hard coded info + remove unnecessary files

--- DA/runner.mjs
+++ DA/runner.mjs
@@ -1,62 +1,31 @@
-import Mocha from 'mocha';
 import { createRequire } from 'module';
-import glob from 'glob'
-
 const require = createRequire(import.meta.url)
+const Mocha = require('mocha');
 const constants = require("../constants");
 const fs = require("fs"),
     path = require("path");
 
 
-let mochaAsync = new Mocha();
 let mocha = new Mocha();
-mocha.reporter('./reporter') // path to custom reporter
-mochaAsync.reporter('./reporter') // path to custom reporter
+mocha.reporter('./reporter.js') // path to custom reporter
+mocha.timeout("10000")
 
-async function sh(cmd) {
-  return new Promise((resolve, reject) => {
-    exec(cmd, {maxBuffer: 1024 * 1000}, (err, stdout, stderr) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve({ stdout, stderr })
-      }
-    })
+let testDir = constants.REPO_PATH + path.sep + constants.REPO_TEST_RELATIVE_DIR
+addFiles(testDir);
+
+mocha.loadFilesAsync().then(()=>{
+  mocha.run((failures)=>{
+    process.exitCode = failures ? 1 : 0
   })
+}).catch(console.error);
+
+function addFiles(dirPath) {
+  fs.readdirSync(dirPath).forEach(filename => {
+    if (fs.statSync(dirPath + path.sep + filename).isDirectory() && filename!="fixtures") {
+      addFiles(dirPath + path.sep + filename)
+    } else if (filename.endsWith('.js') || filename.endsWith('.cjs') || filename.endsWith('.ts')) {
+      console.log(filename);  
+      mocha.addFile(path.join(dirPath, filename));
+      }
+  });
 }
-
-(async function run() {  
-    let testDir = constants.REPO_PATH + path.sep + constants.REPO_TEST_RELATIVE_DIR
-    const testsRegex = `${testDir}/**/*(*.cjs|*.js)`
-    glob(testsRegex, async function(err, files) {
-      // Add each .js file to the mocha instance
-      await Promise.all(files.map(async (file) => {
-        const {stdout, stderr} = await sh(`head -n 10 ${file}`);        
-        if(stdout.includes("import") && stdout.includes("require")){  
-        } else if(stdout.includes("import")){
-            mochaAsync.addFile(file)
-        } else if (stdout.includes("require")){
-            mocha.addFile(file)
-        } else {
-            console.log("ridi", file)
-        }
-      }))  
-    
-      console.log("running mochaAsync")
-      // await mochaAsync.loadFilesAsync()
-      // mochaAsync.run(function (failures) {
-      //   process.exitCode = failures ? 1 : 0 // exit with non-zero status if there were failures
-      // })
-      
-      mochaAsync.loadFilesAsync().then(()=>{
-        mochaAsync.run((failures)=>{
-          process.exitCode = failures ? 1 : 0
-        })
-      }).then(()=>{
-        console.log("running mocha")
-        mocha.run()
-      }).catch(console.error);
-      
-
-    });
-  })().catch(console.error)
